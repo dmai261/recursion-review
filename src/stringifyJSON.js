c4f962ede59b0df33db57cb6780ac34e097b6107
dmai261 <dxmai1314@gmail.com>
2018-07-30 12:13:05 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"'+obj+'"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return ''+obj;
+  } else if (Array.isArray(obj)) {
+    var array = [];
+    for (var elem of obj) {
+      array.push(stringifyJSON(elem));
+    }
+    return '['+array.join(',')+']'
+  } else if (typeof obj === 'object') {
+    var array = [];
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        array.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{'+array.join(',')+'}'
+  }
 };
+
+// var stringifyJSON = function(obj) {
+//   if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+//     return '' + obj;
+//   } else if (typeof obj === 'string') {
+//     return '"' + obj + '"';
+//   } else if ()
+// }
